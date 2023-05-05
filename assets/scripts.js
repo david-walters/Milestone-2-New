a99b2c5e1ffef45b74ddc1baf5850c25bac9a8b6
david-walters <dawudwalters@gmail.com>
2023-05-05 17:28:27 +0100

    Added the script file and began the script
    I have started with the Start button — making this execute the startGame function which brings a random question from the questions array, removes the hide class from question container to reveal the question and answer buttons, and applies the hide class to the start game button

---
+++ assets/scripts.js
@@ -0,0 +1,41 @@
+const startButton = document.getElementById('start-btn');
+const questionContainerElement = document.getElementById('question-container'); 
+const questionElement = document.getElementById('question');
+const answerButtonsElement = document.getElementById('answer-buttons');
+
+let shuffledQuestions, currentQuestionIndex;
+
+startButton.addEventListener('click', startGame);
+
+function startGame() {
+    console.log('Started');
+    startButton.classList.add('hide');
+    shuffledQuestions = questions.sort(() => Math.random() - .5);
+    currentQuestionIndex = 0;
+    questionContainerElement.classList.remove('hide');
+    setNextQuestion();
+}
+
+function setNextQuestion() {
+showQuestion(shuffledQuestions[currentQuestionIndex])
+}
+
+function showQuestion(question) {
+    questionElement.innerText = question.question
+}
+
+function selectAnswer() {
+
+}
+
+const questions = [
+    {
+        question: 'What is 6 + 7?',
+        answers: [
+            {text: '13', correct: true},
+            {text: '67', correct: false},
+            {text: '12', correct: false},
+            {text: '13.5', correct: false},
+        ]
+    }
+]
